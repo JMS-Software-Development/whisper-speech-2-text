be1382d6a57da4811df9d7a0a032bdc11bdbc0af
Jonathan Raes <jonathan@medianasoftware.nl>
2023-01-18 19:21:52 +0100

    docker

--- backend/app.py
+++ backend/app.py
@@ -32,3 +32,12 @@ def transcribe():
         return result['text']
     else:
         return "This endpoint only processes POST wav blob"
+
+
+@app.route('/test', methods=['GET'])
+def test():
+    return "Yolo"
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8000))
+    app.run(debug=True, host='0.0.0.0', port=port)
