1e7011aa52775984b95e003d0e14b0b0f1a36a12
MilanSas <milansas1@gmail.com>
2023-01-23 13:58:37 +0100

    Mic recording imax time and fixes

--- backend/mic_recording.py
+++ backend/mic_recording.py
@@ -10,63 +10,77 @@ from pydub import AudioSegment
 import speech_recognition as sr
 import whisper
 import tempfile
+import datetime
 import os
 import requests
 from time import perf_counter
 
-print("All connected microphones: ", sr.Microphone.list_microphone_names())
-print("All connected microphones: ", str(sr.Microphone.list_working_microphones()))
+for index, mic in enumerate(sr.Microphone.list_microphone_names()):
+    print("Microphone " +str(index) +": "+str(mic))
 
 parser = argparse.ArgumentParser(
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-parser.add_argument("--mic_index", default=1, help="index corresponding to microphone input for audio recording", type=int )
+parser.add_argument("--mic_index", help="index corresponding to microphone input for audio recording", type=int )
 parser.add_argument("--energy", default=500,
                     help="Energy level for mic to detect", type=int)
 parser.add_argument("--dynamic_energy", default=False,
                     help="Flag to enable dynamic energy", type=bool)
 parser.add_argument("--pause", default=0.8,
                     help="Minimum length of silence (sec) that will register as the end of a phrase", type=float)
+# parser.add_argument("--verbose", default=False,
+#                     help="Whether to print verbose output", type=bool)
 args = parser.parse_args()
 
 
-temp_dir = "recordings_" + str(args.mic_index)
-save_path = os.path.join(temp_dir, "temp.wav")
-
-def check_stop_word(predicted_text: str) -> bool:
-    import re
-    pattern = re.compile('[\W_]+', re.UNICODE) 
-    return pattern.sub('', predicted_text).lower() == args.stop_word
+#create folder tmp with mic index
+folder_name = "mic" + str(args.mic_index)
+os.makedirs(folder_name, exist_ok=True)
+save_path = folder_name
 
 def transcribe():
+
+    # for testing
+    # audio_model = whisper.load_model('tiny')
+
     # load the speech recognizer with CLI settings
     r = sr.Recognizer()
     r.energy_threshold = args.energy
     r.pause_threshold = args.pause
     r.dynamic_energy_threshold = args.dynamic_energy
-    # TODO:check how it knows when sentence is done
-    # TODO:whene max time is reached, stop sentence
-    # TODO: How does with work?
     with sr.Microphone(sample_rate=16000, device_index=args.mic_index) as source:
         print("Let's get the talking going! Listening to mic: ", args.mic_index)
+        start_time = datetime.datetime.now()
         while True:
             # record audio stream into wav
-            audio = r.listen(source)
+            audio = r.listen(source, phrase_time_limit=10)
+            #print time delta
+            print("Time delta: ", datetime.datetime.now()-start_time)
             start = perf_counter()
             data = io.BytesIO(audio.get_wav_data())
             audio_clip = AudioSegment.from_file(data)
+            save_path = os.path.join(folder_name, "recording"+datetime.datetime.now().isoformat()+".wav")
             audio_clip.export(save_path, format="wav")
             
 
-            #TODO create request to backend
-            files = {'upload_file': open("recordings_" + str(args.mic_index) + "/temp.wav",'rb')}
-            url = ""
-            requests.post(url, files=files)
+            # #TODO create request to backend
+            # files = {'upload_file': open("recordings_" + str(args.mic_index) + "/temp.wav",'rb')}
+            # url = ""
+            # requests.post(url, files=files)
+            
+            # for testing
+            # result = audio_model.transcribe(save_path, language="dutch")
 
-            
-            
-            
-
+            # if not args.verbose:
+            #     predicted_text = result["text"]
+            #     print("Text: " + predicted_text)
+            # else:
+            #     predicted_text = result["text"]
+            #     end = perf_counter()
+            #     duration = end-start
+            #     for k,v in result.items():
+            #         print(k,v)
+            #     print("Processing delay: ", duration)
 
 if __name__ == "__main__":
     transcribe()
