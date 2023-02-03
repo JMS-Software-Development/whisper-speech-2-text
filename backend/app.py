e4c8ab2ccaa792c072b19720ac127d1c556b4ec5
MilanSas <milansas1@gmail.com>
2023-02-03 16:40:47 +0100

    backend with queud wave files

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,10 @@
 import os
+from queue import Queue
 import tempfile
 import flask
 from flask import request
+import datetime
+from threading import Thread
 from flask_cors import CORS
 import whisper
 import torch
@@ -9,47 +12,68 @@ import time
 
 app = flask.Flask(__name__)
 CORS(app)
-
-
 class DataStore():
     language = "dutch"
-    model = "medium"
+    model = "small"
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using device: ", device)
     audio_model = whisper.load_model(model)
+
 
 t = time.time()
 data = DataStore()
 print(f"Loading model took {time.time() - t} seconds")
 
+dataQueue = Queue()
+
+def transcribe_thread():
+    print("Starting transcribing thread")
+    while True:
+        if dataQueue.empty():
+            print("No audio data in queue")
+            time.sleep(1)
+        else:
+            start_time = time.time()
+            print("Startinhg transcribing")
+            audio_model = data.audio_model
+            save_path = dataQueue.get()
+
+            result = audio_model.transcribe(save_path, language='dutch')
+
+            print(result['text'])
+            print(f"Transcribing took {time.time() - start_time} seconds")
+            print(result['text'])
+
+transcriber = Thread(target=transcribe_thread)
+transcriber.start()
+folder_name = "recordings"
+os.makedirs(folder_name, exist_ok=True)
+
 @app.route('/transcribe', methods=['POST'])
-def transcribe():
+def addToQueue():
     if request.method == 'POST':
         #language = request.form['language']
         #model = request.form['model_size']
-        start_time = time.time()
-        print("Startinhg transcribing")
-        audio_model = data.audio_model
-
-        temp_dir = tempfile.mkdtemp()
-        save_path = os.path.join(temp_dir, 'temp.wav')
-
+        save_path = os.path.join(folder_name, "recording"+datetime.datetime.now().isoformat()+".wav")
         wav_file = request.files['audio_data']
         wav_file.save(save_path)
+        dataQueue.put(save_path)
 
-        result = audio_model.transcribe(save_path, language='dutch')
-
-        print(result['text'])
-        print(f"Transcribing took {time.time() - start_time} seconds")
-        return result['text']
+        print("Added to queue")
+        print(dataQueue.qsize())
+        return "Added to queue"
     else:
         return "This endpoint only processes POST wav blob"
-
-
-@app.route('/test', methods=['GET'])
-def test():
-    return "Yolo"
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8000))
     app.run(debug=True, host='0.0.0.0', port=port)
+
+
+
+
+
+    
+
+
+
