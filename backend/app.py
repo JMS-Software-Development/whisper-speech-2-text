21e7f547de97ad48b48012b6bb746a9f93b50f71
MilanSas <milansas1@gmail.com>
2023-03-16 14:55:43 +0100

    set default model to large

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,7 @@ CORS(app)
 class DataStore():
     language = "dutch"
     # print("models available", whisper.available_models())
-    model = "tiny" #tiny, base, small, medium, large, large-v1, large-v2
+    model = "large" #tiny, base, small, medium, large, large-v1, large-v2
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print("Using device: ", device)
     audio_model = whisper.load_model(model)
@@ -53,7 +53,7 @@ def transcribe_thread():
                 save_path = os.path.join(TRANSSCRIPTIONS_FOLDER, recording_id + "-" + datetime.datetime.now().isoformat()+".txt")
                 with open(save_path, 'w') as f:
                     f.write(result['text'].strip())
-
+            
             if time.time() - start_generating > 240:
                 start_generating = time.time()
                 generateImage(result['text'])
